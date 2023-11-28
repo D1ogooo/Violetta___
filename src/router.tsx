2acc11b7b21d048b1df982a5e2b5d6514457dc24
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 17:24:37 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,32 +1,36 @@
-import PostsUsuario from './pages/Dashboard/MinhasPostagens/PostsDoUsuario'
-import MeusLikes from './pages/Dashboard/MeusDados/MeusLikesPerfil' 
-import Home from './pages/Home/PrimeiraParte/HomePage'
-import Login from './pages/Login'
-import TokenAuthenticate from './pages/TokenAuthenticate/index'
-import Register from './pages/Register'
-import { createBrowserRouter } from 'react-router-dom'
+import PostsUsuario from './pages/Dashboard/MinhasPostagens/PostsDoUsuario';
+import MeusLikes from './pages/Dashboard/MeusDados/MeusLikesPerfil';
+import Home from './pages/Home/PrimeiraParte/HomePage';
+import Login from './pages/Login';
+import TokenAuthenticate from './pages/TokenAuthenticate/index';
+import Register from './pages/Register';
+import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
-{
- path: "/",
- element: <Home/>,
-},
-{
- path: '/login',
- element: <Login />,
-},
-{
- path: "/register",
- element: <Register />,
-},
-{
- path: "/authToken",
- element: <TokenAuthenticate />,
-},
-{
- path: "/dashboard", // Sei que está errado, porem ainda não temos banco
- element: <PostsUsuario /> // vai fica assim por enquanto
-},
-])
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/authToken',
+    element: <TokenAuthenticate />,
+  },
+  {
+    path: '/dashboard', // Sei que está errado, porem ainda não temos banco
+    element: <PostsUsuario />, // vai ficar assim por enquanto
+  },
+  {
+    path: '/meusLikes', // Adicionando uma rota para MeusLikes
+    element: <MeusLikes />,
+  },
+]);
 
-export default router
+export default router;
