7b1913d467781f68a354751144bd3a52f2cba5b5
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 17:08:46 -0300

    Update HomeSegundaParte.tsx

--- src/pages/Home/SegundaParte/HomeSegundaParte.tsx
+++ src/pages/Home/SegundaParte/HomeSegundaParte.tsx
@@ -2,9 +2,6 @@ import { PaiContainer, LeftContainer, RightContainer, Title } from './HomeStyle'
 import { PrimeiraColuna, SegundaColuna, CadeadoImg, RetangleImg } from './HomeStyle'
 import CadeadoImage from './assets/cadeado.svg'
 import RetangleImage from './assets/Rectangle com borda branca que não estou com disposição para quebrar a cabeça para fazer porque estou nervososvg.svg'
-import FirstImage from '../../../assets/Images/DreamShaper_v7_ANIME_STYLE_friends_on_a_park_playing_around_wi_0.jpg'
-import SecondImage from '../../../assets/Images/DreamShaper_v7_anime_girl_with_a_hoddie_smiling_with_eyes_clos_0.jpg'
-
 
 export default function HomeSegunda() {
     return (
