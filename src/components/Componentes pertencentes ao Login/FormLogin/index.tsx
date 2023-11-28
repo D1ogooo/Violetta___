9d9acf77810b34731c1bbc9d9b89c44b2eb20526
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 16:56:07 -0300

    Update index.tsx

--- src/components/Componentes pertencentes ao Login/FormLogin/index.tsx
+++ src/components/Componentes pertencentes ao Login/FormLogin/index.tsx
@@ -15,7 +15,11 @@ export default function FormLogin() {
         password,
       });
 
-      navigate('/dashboard');
+      if (response.data.status === 200) {
+        navigate('/dashboard');
+      } else {
+        console.log("Login failed"); // You might want to add some feedback here
+      }
     } catch (error) {
       console.error(error);
     }
@@ -29,13 +33,17 @@ export default function FormLogin() {
     setPassword(event.target.value);
   }
 
+  function HandleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
     <S.ContainerPai>
       <S.Titulo>LOGIN</S.Titulo>
-      <S.FormContainer>
+      <S.FormContainer onSubmit={HandleSubmit}>
         <S.Input value={email} type="email" onChange={HandleEmail} placeholder='Insira o seu email...' />
         <S.Input value={password} type='password' onChange={HandlePassword} placeholder='Insira a sua senha... ' />
-        <S.SubmitButton type='button' onClick={LoginSubmit} disabled={password.length > 10 || password.length === 0}>
+        <S.SubmitButton type='submit' onClick={LoginSubmit} disabled={password.length > 10 || password.length === 0}>
           ENTRAR
         </S.SubmitButton>
       </S.FormContainer>
