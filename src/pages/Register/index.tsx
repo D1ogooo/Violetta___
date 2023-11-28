78a976629b34c38c4cafddf53d93d876707b68f1
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 17:48:42 -0300

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -13,69 +13,69 @@ export default function Register() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
   
-   function registerSubmit () {
+  function registerSubmit() {
     axios.post('https://back-end-production-5622.up.railway.app/user', {
-     "email": `${email}`,
-     "password": `${password}`,
-     "passwordConfirmation": `${confirmPassword}`,
+      "email": `${email}`,
+      "password": `${password}`,
+      "passwordConfirmation": `${confirmPassword}`,
     })
-
     .then((res) => {
-      res.data.status === 200 && navigate('/authToken') :  
-    });
-
+      if (res.data.status === 200) {
+        navigate('/authToken');
+      }
+    })
     .catch(function (error) {
-     console.log(error);
+      console.log(error);
     });
   }
 
   function HandleEmail(event: React.ChangeEvent<HTMLInputElement>) {
-   setEmail(event.target.value);
+    setEmail(event.target.value);
   }
 
   function HandlePassword(event: React.ChangeEvent<HTMLInputElement>) {
-   setPassword(event.target.value);
+    setPassword(event.target.value);
   }
 
   function HandleConfirmPassword(event: React.ChangeEvent<HTMLInputElement>) {
-   setConfirmPassword(event.target.value);
+    setConfirmPassword(event.target.value);
   }
 
   function HandleSubmit(event: React.FormEvent<HTMLFormElement>) {
-   event.preventDefault();
+    event.preventDefault();
   }
 
-    return (
-        <>
-            <GlobalStyle />
-            <Header />
-            <S.ContainerPai>
-                <S.Titulo>REGISTRO</S.Titulo>
-                <S.FormContainer onSubmit={HandleSubmit}>
-                    <S.Input value={email} onChange={HandleEmail} type="email" placeholder="Insira o seu email..." required />
-                    <S.Input value={password} onChange={HandlePassword} type="password" placeholder="Insira a sua senha..." required />
-                    <S.Input value={confirmPassword} onChange={HandleConfirmPassword} type="password" placeholder="Repita a sua senha..." required />
-                    <S.SubmitButton type="submit" disabled={password != confirmPassword || password.length < 8 || password.length < 1} onClick={registerSubmit}>CRIAR CONTA</S.SubmitButton>
-                    {password.length != confirmPassword.length ? (
-                      <S.Notification_register>
-                         <S.Notification__body_register>
-                          <IoIosClose/> As senhas não coincidem
-                          </S.Notification__body_register>
-                        <S.Notification__progress_register></S.Notification__progress_register>
-                      </S.Notification_register>
-                    ) : null}
-                    {password.length > 10 ? (
-                      <S.Notification_register>
-                         <S.Notification__body_register>
-                         <IoIosClose/> A senha não pode ter menos de 8 carácteres
-                          </S.Notification__body_register>
-                        <S.Notification__progress_register></S.Notification__progress_register>
-                      </S.Notification_register>
-                    ) : null}
-                </S.FormContainer>
-                <S.LinkTo to="/login">Já tem conta? Faça o login!</S.LinkTo>
-            </S.ContainerPai>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <S.ContainerPai>
+        <S.Titulo>REGISTRO</S.Titulo>
+        <S.FormContainer onSubmit={HandleSubmit}>
+          <S.Input value={email} onChange={HandleEmail} type="email" placeholder="Insira o seu email..." required />
+          <S.Input value={password} onChange={HandlePassword} type="password" placeholder="Insira a sua senha..." required />
+          <S.Input value={confirmPassword} onChange={HandleConfirmPassword} type="password" placeholder="Repita a sua senha..." required />
+          <S.SubmitButton type="submit" disabled={password !== confirmPassword || password.length < 8 || password.length < 1} onClick={registerSubmit}>CRIAR CONTA</S.SubmitButton>
+          {password.length !== confirmPassword.length ? (
+            <S.Notification_register>
+              <S.Notification__body_register>
+                <IoIosClose/> As senhas não coincidem
+              </S.Notification__body_register>
+              <S.Notification__progress_register></S.Notification__progress_register>
+            </S.Notification_register>
+          ) : null}
+          {password.length > 10 ? (
+            <S.Notification_register>
+              <S.Notification__body_register>
+                <IoIosClose/> A senha não pode ter menos de 8 caracteres
+              </S.Notification__body_register>
+              <S.Notification__progress_register></S.Notification__progress_register>
+            </S.Notification_register>
+          ) : null}
+        </S.FormContainer>
+        <S.LinkTo to="/login">Já tem conta? Faça o login!</S.LinkTo>
+      </S.ContainerPai>
+      <Footer />
+    </>
+  );
 }
