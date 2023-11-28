bcfe081ef4396efe4a4ded02eade4eda16597e77
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 17:07:09 -0300

    Update PostsData.ts

--- src/pages/Dashboard/MinhasPostagens/Data/PostsData.ts
+++ src/pages/Dashboard/MinhasPostagens/Data/PostsData.ts
@@ -1,4 +1,4 @@
-import ImagemExemplo from '../assets/Imagen de carro - exemplo, não sera usada provavelmente.svg'
+// import ImagemExemplo from '../assets/Imagen de carro - exemplo, não sera usada provavelmente.svg'
 
 interface PostsType {
     title: string,
@@ -7,6 +7,7 @@ interface PostsType {
 }
 
 export const Posts: PostsType[] = [
+    // ... Outros itens
     // {
     //   title: "Título",
     //   text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat,odit, tenetur error, harum nesciunt ipsum debitis quas aliquid.",
