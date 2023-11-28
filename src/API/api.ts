0711c239e756fe1399a3c031a81426c6d0a80f1c
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-28 13:48:55 -0300

    Update api.ts

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,11 +1,1 @@
-import axios from 'axios'
-
-export const api = axios.create({
- baseURL: 'https://back-end-production-5622.up.railway.app/user/'
-})
- 
-api.interceptors.request.use(async (config) => {
- config.headers.authorization = `Bearer edkso92cnPOiuwND82912ohndcmspo92`
- return config
-})
 
